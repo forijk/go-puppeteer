70499de7ffb078f9b2fd831954d1c8ca4feb888c
forijk <forijk@163.com>
2019-11-22 16:37:23 +0800

    completed.

--- download.js
+++ download.js
@@ -1,15 +1,23 @@
 /**
  * 下载掘金【我的收藏集】文章为 PDF 文件
- * 含对图片 lazyload 的处理
+ * 含对懒加载图片 lazyload 的处理，默认开启
  * SOURCE_URL 为【我的收藏集】链接
  */
 const puppeteer = require('puppeteer');
 const mkdirp = require('mkdirp');
 const axios = require('axios');
+const ProgressBar = require('progress');
+
 const BASE_URL = 'https://juejin.im';
 const SOURCE_URL = 'https://juejin.im/collection/5dd56bd0e51d4505fd7facd2';
 
-const printJuejinBooks = async (userName, password, saveDir = './books') => {
+
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
+});
+
+const printJuejinBooks = async (userName, password, isLazyload = true, saveDir = './books') => {
   if (!userName) {
     throw new Error('请输入用户名');
   }
@@ -17,43 +25,15 @@ const printJuejinBooks = async (userName, password, saveDir = './books') => {
     throw new Error('请输入密码');
   }
   try {
-    // 默认网站图片为懒加载方式
-    const config = {
-      lazyLoad: true
-    }
-    const result = {
-      status: 'OK'
-    };
     // 设置统一的视窗大小
     const viewport = {
       width: 1376,
       height: 768
     };
 
-    /**
-     * Logs json to the console and if required terminates the process
-     *
-     * @param status
-     * @param message
-     * @param terminate
-     */
-    function log(status, message, terminate) {
-      result.status = status;
-
-      if (message) {
-        result.message = message;
-      }
-
-      console.log(result);
-
-      if (terminate) {
-        process.exit(terminate);
-      }
-    }
-
     console.log('启动浏览器');
     const browser = await puppeteer.launch({
-      // 关闭无头模式，方便我们看到整个无头浏览器执行的过程
+      // 关闭无头模式，便于看到整个无头浏览器的执行过程
       // 注意若调用了 Page.pdf 即保存为 pdf，则需要保持为无头模式
       // headless: false
     });
@@ -101,74 +81,63 @@ const printJuejinBooks = async (userName, password, saveDir = './books') => {
     });
     console.log(`收藏集上共找到${books.length}篇文章:`);
     books.forEach((item, index) => {
-      console.log(index + 1 + '.' + item.title);
+      console.log('  ', index + 1 + '.' + item.title);
     });
 
     for (let article of books) {
       const articlePage = await browser.newPage();
       articlePage.setViewport(viewport);
-      await articlePage.goto(`${BASE_URL}${article.href}`, {
-        waitUtil: 'networkidle2'
-      });
 
-      if (config.lazyLoad) {
+      await articlePage.goto(`${BASE_URL}${article.href}`);
+
+      if (isLazyload) {
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+        console.log('开始图片懒加载识别, 请稍后...');
         var maxScroll = await articlePage.evaluate(() => {
           return Promise.resolve(
             Math.max(document.body.scrollHeight, document.body.offsetHeight,
               document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight) - window.innerHeight
           );
-        }).catch(function () {
-          log('BAD', 'Lazy load failed due to an error while getting the scroll height.', 1);
+        }).catch((err) => {
+          console.log(err);
         });
-        console.log("TCL: printJuejinBooks -> maxScroll", maxScroll)
 
         // how many times full scroll needs to be done
         var fullScrolls = Math.floor(maxScroll / viewport.height);
-        console.log("TCL: printJuejinBooks -> fullScrolls", fullScrolls)
-
+        console.log("预计总用时: ", fullScrolls, 's')
         // amount left to get to the bottom of the page after doing the full scrolls
         var lastScroll = maxScroll % viewport.height;
-        console.log("TCL: printJuejinBooks -> lastScroll", lastScroll)
+        // progress
+        var bar = new ProgressBar('  Timing [:bar] :percent :etas', {
+          complete: '=',
+          incomplete: ' ',
+          total: fullScrolls
+        });
 
         // do full scrolls if there is any
         for (var i = 1; i <= fullScrolls; i++) {
-
-          console.log("TCL: printJuejinBooks -> i", i)
-
           await articlePage.evaluate((i, viewportHeight) => {
             return Promise.resolve(window.scrollTo(0, i * viewportHeight));
-          }, i, viewport.height).catch(function () {
-            result.status = 'BAD';
-            result.message = 'Promise rejected while scrolling for lazy load images.';
+          }, i, viewport.height).catch(err => {
+            console.log(err);
           });
-
-          await articlePage.waitForNavigation({
-            // 'waitUntil': 'networkidle2',
-            // 'networkIdleTimeout': 200
-          }).catch(function () {
-            result.status = 'BAD';
-            result.message = 'Promise rejected while waiting for navigation 1';
+          await articlePage.waitFor(1000).catch(err => {
+            console.log(err);
           });
+          bar.tick(1);
         }
-
         // do last scroll if there is any
         if (lastScroll > 0) {
           await articlePage.evaluate(maxScroll => {
             return Promise.resolve(window.scrollTo(0, maxScroll + 25));
-          }, maxScroll).catch(function () {
-            result.status = 'BAD';
-            result.message = 'Promise rejected while last scrolling for lazy load images.';
+          }, maxScroll).catch(err => {
+            console.log(err);
           });
         }
-
-        await articlePage.waitForNavigation({
-          'waitUntil': 'networkidle2'
-        }).catch(function (error) {
-          result.status = 'BAD';
-          result.message = 'Promise rejected while waiting for navigation 2';
+        await articlePage.waitFor(2000).catch(err => {
+          console.log(err);
         });
       }
-
       await articlePage.waitForSelector('.main-container');
       await articlePage.$eval('body', body => {
         body.querySelector('.main-header-box').style.display = 'none';
@@ -182,11 +151,11 @@ const printJuejinBooks = async (userName, password, saveDir = './books') => {
         Promise.resolve();
       });
 
-      const dirPath = `${saveDir}/${article.title}`;
       const fileName = `${article.title.replace(/\//g, '、')}.pdf`;
-      const filePath = `${dirPath}/${fileName}`;
-      mkdirp.sync(dirPath);
+      const filePath = `${saveDir}/${fileName}`;
+      mkdirp.sync(saveDir);
 
+      console.log('开始转换为 PDF 文件...');
       await page.emulateMedia('screen');
       await articlePage.pdf({
         path: filePath,
@@ -194,22 +163,24 @@ const printJuejinBooks = async (userName, password, saveDir = './books') => {
         printBackground: true
       });
       console.log(`保存成功: ${filePath}`);
-      articlePage.close();
+      // Protocol error (Target.closeTarget): Target closed.
+      // articlePage.close();
     }
-
+    console.log('恭喜您! 全部下载成功！');
     browser.close();
-  } catch (e) {
-    console.error(e);
+  } catch (err) {
+    console.error(err);
   }
 };
 
 const USER = process.argv[2];
 const PASSWORD = process.argv[3];
 const SAVE_DIR = process.argv[4];
+const IS_LAZYLOAD = process.argv[5];
 
 if (!USER || !PASSWORD) {
   console.log('invalid user or password');
   process.exit();
 }
 
-printJuejinBooks(USER, PASSWORD, SAVE_DIR);
+printJuejinBooks(USER, PASSWORD, IS_LAZYLOAD, SAVE_DIR);
